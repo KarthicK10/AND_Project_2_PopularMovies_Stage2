eb35fd0df2ddee0c52126592b2590bc9045e39e4
KarthicK Mariappan <karthick1.mariappan@gmail.com>
2016-06-05 09:37:15 -0400

    feat: Create grid view

--- app/src/main/java/com/example/karthick/popularmovies/MoviesFragment.java
+++ app/src/main/java/com/example/karthick/popularmovies/MoviesFragment.java
@@ -5,6 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,7 +31,23 @@ public class MoviesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        /*Inflate the grid view*/
         View rootView = inflater.inflate(R.layout.moviesfragment, container, false);
+
+        /*Create dummy data for testing the grid view */
+        ArrayList<String> dummmyData = new ArrayList<>(Arrays.asList(
+                "movie1", "movie2", "movie3", "movie4", "movie5", "movie6"
+        ));
+
+        /*Initiate an array adapter to supply text views to the grids*/
+        ArrayAdapter<String> moviesGridAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.grid_item_movie, R.id.grid_item_movie_textView,
+                dummmyData
+        );
+
+        GridView moviesGrid = (GridView) rootView.findViewById(R.id.gridview_movies);
+        moviesGrid.setAdapter(moviesGridAdapter);
 
         return rootView;
     }
