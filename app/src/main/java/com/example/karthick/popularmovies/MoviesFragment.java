9cf3d568a2c15d1c8108ba23809b2a4b997cad3f
KarthicK Mariappan <karthick1.mariappan@gmail.com>
2016-06-05 08:48:54 -0400

    feat: Add a fragment for showing all the movies

---
+++ app/src/main/java/com/example/karthick/popularmovies/MoviesFragment.java
@@ -0,0 +1,34 @@
+package com.example.karthick.popularmovies;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ * Container for all movies.
+ */
+public class MoviesFragment extends Fragment {
+
+    public MoviesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.moviesfragment, container, false);
+
+        return rootView;
+    }
+
+}
