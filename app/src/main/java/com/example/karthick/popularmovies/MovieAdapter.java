a59c376d0d059179f9713cd8d552168f63aa6c2c
KarthicK Mariappan <karthick1.mariappan@gmail.com>
2016-06-15 21:29:14 -0400

    refactor: Implement ViewHolder pattern

--- app/src/main/java/com/example/karthick/popularmovies/MovieAdapter.java
+++ app/src/main/java/com/example/karthick/popularmovies/MovieAdapter.java
@@ -2,6 +2,7 @@ package com.example.karthick.popularmovies;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +18,15 @@ import java.util.List;
  * Created by KarthicK on 6/6/2016.
  */
 public class MovieAdapter extends ArrayAdapter<Movie> {
+
     private static final String LOG_TAG = MovieAdapter.class.getSimpleName();
+
+    /*View holder to store references to views
+     *  saves resource spent on calls to findViewById calls
+     *  helps smooth scrolling*/
+    private static class MovieViewHolder{
+        ImageView posterImageView;
+    }
 
     public MovieAdapter(Activity context, List<Movie> moviesList){
         super(context, 0, moviesList);
@@ -33,11 +42,23 @@ public class MovieAdapter extends ArrayAdapter<Movie> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        Log.i(LOG_TAG, "view asked");
+
+        MovieViewHolder movieViewHolder;
+
         Context context = getContext();
 
         /*Recycle old view if possible*/
         if(convertView == null){
+            movieViewHolder = new MovieViewHolder();
             convertView = LayoutInflater.from(context).inflate(R.layout.grid_item_movie, parent, false);
+            movieViewHolder.posterImageView = (ImageView)convertView.findViewById(R.id.grid_item_movie_poster_imageView);
+            convertView.setTag(movieViewHolder);
+            Log.i(LOG_TAG, "view created");
+        }
+        else{
+            Log.i(LOG_TAG, "view recycled");
+            movieViewHolder = (MovieViewHolder)convertView.getTag();
         }
         Movie movie = getItem(position);
         String posterPath = movie.getPosterPath();
@@ -45,11 +66,15 @@ public class MovieAdapter extends ArrayAdapter<Movie> {
         String imageSizePath = context.getString(R.string.mdb_image_size_500);
 
         /*Load the image view by fetching image from picasso using the movie's poster path */
-        ImageView posterImageView = (ImageView)convertView.findViewById(R.id.grid_item_movie_poster_imageView);
+        //ImageView posterImageView = (ImageView)convertView.findViewById(R.id.grid_item_movie_poster_imageView);
         String picassoPath = IMAGE_BASE_URL+imageSizePath+movie.getPosterPath();
-        Picasso.with(context).load(picassoPath).into(posterImageView);
+        Picasso.with(context).load(picassoPath).into(movieViewHolder.posterImageView);
+
+        Log.i(LOG_TAG, "view returned");
 
         return convertView;
+
+
     }
 }
 
