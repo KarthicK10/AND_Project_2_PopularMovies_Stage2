26b9f9ed5cf6bb8f46fdfc24da19a76e57545df0
KarthicK Mariappan <karthick1.mariappan@gmail.com>
2016-10-20 17:43:40 -0400

    feat: Implement LoaderCallbacks. Parallely load from web and db and populate screen. To be fixed later to chose between web or db based on settings.

--- app/src/main/java/com/example/karthick/popularmovies/MoviesFragment.java
+++ app/src/main/java/com/example/karthick/popularmovies/MoviesFragment.java
@@ -1,21 +1,31 @@
 package com.example.karthick.popularmovies;
 
+import android.content.Context;
+import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CursorAdapter;
 
 import com.example.karthick.popularmovies.data.Movie;
+import com.example.karthick.popularmovies.data.MovieContract;
 import com.example.karthick.popularmovies.data.MovieDBResult;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -28,7 +38,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  *
  * Container for all movies.
  */
-public class MoviesFragment extends Fragment {
+public class MoviesFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>{
 
     private static final String LOG_TAG = MoviesFragment.class.getSimpleName();
     private static final String MOVIE_API_BASE_URL = "https://api.themoviedb.org";
@@ -39,6 +49,30 @@ public class MoviesFragment extends Fragment {
 
     //Sort order Key for storing onSaveInstanceState
     public static final String SORT_ORDER = "sort_order";
+
+    //Loader ID
+    private static final int MOVIE_LOADER = 1;
+
+    private static final String[] FAVORITE_MOVIE_COLUMNS = {
+            MovieContract.FavoriteEntry._ID,
+            MovieContract.FavoriteEntry.COLUMN_MOVIE_ID,
+            MovieContract.FavoriteEntry.COLUMN_ORIGINAL_TITLE,
+            MovieContract.FavoriteEntry.COLUMN_POSTER_PATH,
+            MovieContract.FavoriteEntry.COLUMN_BACKDROP_PATH,
+            MovieContract.FavoriteEntry.COLUMN_SYNAPSIS,
+            MovieContract.FavoriteEntry.COLUMN_VOTE_AVERAGE,
+            MovieContract.FavoriteEntry.COLUMN_RELEASE_DATE
+    };
+
+    //These indices are tied to FAVORITE_MOVIE_COLUMNS !Important
+    static final int COL_FAVORITE_ID = 0;
+    static final int COL_MOVIE_ID = 1;
+    static final int COL_ORIGINAL_TITLE = 2;
+    static final int COL_POSTER_PATH = 3;
+    static final int COL_BACKDROP_PATH = 4;
+    static final int COL_SYNAPSIS = 5;
+    static final int COL_VOTE_AVERAGE = 6;
+    static final int COL_RELEASE_DATE = 7;
 
     public MoviesFragment() {
         // Required empty public constructor
@@ -127,6 +161,25 @@ public class MoviesFragment extends Fragment {
         prevSortOrder = getSortOrderPath();
         Log.i("Saving sort order : ", getSortOrderPath());
         outState.putString(SORT_ORDER, prevSortOrder);
+    }
+
+    /**
+     * Called when the fragment's activity has been created and this
+     * fragment's view hierarchy instantiated.  It can be used to do final
+     * initialization once these pieces are in place, such as retrieving
+     * views or restoring state.  It is also useful for fragments that use
+     * {@link #setRetainInstance(boolean)} to retain their instance,
+     * as this callback tells the fragment when it is fully associated with
+     * the new activity instance.  This is called after {@link #onCreateView}
+     * and before {@link #onViewStateRestored(Bundle)}.
+     *
+     * @param savedInstanceState If the fragment is being re-created from
+     *                           a previous saved state, this is the state.
+     */
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        getLoaderManager().initLoader(MOVIE_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
     }
 
     /*Method to update the movies by calling theMovieDB API */
@@ -237,4 +290,94 @@ public class MoviesFragment extends Fragment {
         return prefs.getString(getString(R.string.pref_sort_order_key), getString(R.string.pref_sort_order_default));
     }
 
+    /**
+     * Instantiate and return a new Loader for the given ID.
+     *
+     * @param id   The ID whose loader is to be created.
+     * @param args Any arguments supplied by the caller.
+     * @return Return a new Loader instance that is ready to start loading.
+     */
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Uri favoriteMovieUri = MovieContract.FavoriteEntry.buildFavoriteUri();
+        return new android.support.v4.content.CursorLoader(getActivity(),
+                favoriteMovieUri,
+                FAVORITE_MOVIE_COLUMNS,
+                null,
+                null,
+                null);
+    }
+
+    /**
+     * Called when a previously created loader has finished its load.  Note
+     * that normally an application is <em>not</em> allowed to commit fragment
+     * transactions while in this call, since it can happen after an
+     * activity's state is saved.  See {@link android.app.FragmentManager#beginTransaction()
+     * FragmentManager.openTransaction()} for further discussion on this.
+     * <p>
+     * <p>This function is guaranteed to be called prior to the release of
+     * the last data that was supplied for this Loader.  At this point
+     * you should remove all use of the old data (since it will be released
+     * soon), but should not do your own release of the data since its Loader
+     * owns it and will take care of that.  The Loader will take care of
+     * management of its data so you don't have to.  In particular:
+     * <p>
+     * <ul>
+     * <li> <p>The Loader will monitor for changes to the data, and report
+     * them to you through new calls here.  You should not monitor the
+     * data yourself.  For example, if the data is a {@link Cursor}
+     * and you place it in a {@link CursorAdapter}, use
+     * the {@link CursorAdapter#CursorAdapter(Context,
+     * Cursor, int)} constructor <em>without</em> passing
+     * in either {@link CursorAdapter#FLAG_AUTO_REQUERY}
+     * or {@link CursorAdapter#FLAG_REGISTER_CONTENT_OBSERVER}
+     * (that is, use 0 for the flags argument).  This prevents the CursorAdapter
+     * from doing its own observing of the Cursor, which is not needed since
+     * when a change happens you will get a new Cursor throw another call
+     * here.
+     * <li> The Loader will release the data once it knows the application
+     * is no longer using it.  For example, if the data is
+     * a {@link Cursor} from a {@link CursorLoader},
+     * you should not call close() on it yourself.  If the Cursor is being placed in a
+     * {@link CursorAdapter}, you should use the
+     * {@link CursorAdapter#swapCursor(Cursor)}
+     * method so that the old Cursor is not closed.
+     * </ul>
+     *
+     * @param loader The Loader that has finished.
+     * @param data   The data generated by the Loader.
+     */
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        ArrayList<Movie> moviesList = new ArrayList<>();
+        if(data.moveToFirst()){
+            do{
+                moviesList.add(new Movie(
+                        data.getInt(COL_MOVIE_ID),
+                        data.getString(COL_ORIGINAL_TITLE),
+                        data.getString(COL_POSTER_PATH),
+                        data.getString(COL_BACKDROP_PATH),
+                        data.getString(COL_SYNAPSIS),
+                        data.getDouble(COL_VOTE_AVERAGE),
+                        new Date() //TODO get actual release date
+                ));
+            }while(data.moveToNext());
+            mMoviesList.clear();
+            mMoviesList.addAll(moviesList);
+            moviesGridAdapter.notifyDataSetChanged();
+        }
+    }
+
+    /**
+     * Called when a previously created loader is being reset, and thus
+     * making its data unavailable.  The application should at this point
+     * remove any references it has to the Loader's data.
+     *
+     * @param loader The Loader that is being reset.
+     */
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mMoviesList.clear();
+        moviesGridAdapter.notifyDataSetChanged();
+    }
 }
